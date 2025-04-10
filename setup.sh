baab20ccbbbb0d1b2efef071e5b54ef9c5393d67
Jason Hwang <173634806+dotjasonhwang@users.noreply.github.com>
2025-04-10 13:31:08 -0500

    Make script run from other locations

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Get the directory of the script
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
 # Check if PROJECT_ROOT is provided as an argument
 if [ -z "$1" ]; then
   echo "Usage: $0 <project_root> [optional_readme_file]"
@@ -7,8 +10,8 @@ if [ -z "$1" ]; then
 fi
 
 PROJECT_ROOT=$1
-TEMPLATE_README=${2:-BLANK_README.md}
+TEMPLATE_README=${2:-"$SCRIPT_DIR/BLANK_README.md"}
 
 cp "$TEMPLATE_README" "$PROJECT_ROOT/README.md"
-cp -r .github "$PROJECT_ROOT"
-cp -r readme-assets "$PROJECT_ROOT"
+cp -r "$SCRIPT_DIR/.github" "$PROJECT_ROOT"
+cp -r "$SCRIPT_DIR/readme-assets" "$PROJECT_ROOT"
